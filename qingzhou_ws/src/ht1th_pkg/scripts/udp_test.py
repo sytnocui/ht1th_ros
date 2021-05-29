f096fbb6c5dad9d3e81d7105de22bfa92012e5d3
sytnocui <1079851849@qq.com>
2021-05-29 22:15:56 +0800

    实现udp_test.py实时参数查看

--- qingzhou_ws/src/ht1th_pkg/scripts/udp_test.py
+++ qingzhou_ws/src/ht1th_pkg/scripts/udp_test.py
@@ -6,20 +6,34 @@ import sys
 import rospy
 from std_srvs.srv import Empty
 
-if __name__=="__main__":
+import struct
+import socket
+import time
 
+if __name__=="__main__":
     rospy.init_node("viewpara",anonymous=True)
 
-    viewpara = []
+    #client 发送端
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ("127.0.0.1", 8888)  # 接收方 服务器的ip地址和端口号
 
-    #读取参数服务器
-    viewpara[0] = rospy.get_param("/ht1th/viewpara/robot_state")
-    viewpara[1] = rospy.get_param("/ht1th/viewpara/visual_nav")
-    viewpara[2] = rospy.get_param("/ht1th/viewpara/pose_x")
-    viewpara[3] = rospy.get_param("/ht1th/viewpara/pose_y")
-    viewpara[4] = rospy.get_param("/ht1th/viewpara/pose_yaw")
-    viewpara[5] = rospy.get_param("/ht1th/viewpara/vel_left")
-    viewpara[6] = rospy.get_param("/ht1th/viewpara/vel_right")
-    viewpara[7] = rospy.get_param("/ht1th/viewpara/vel_angle")
+    viewpara = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
-    rospy.loginfo(viewpara)
+    while True:
+        #读取参数服务器
+        viewpara[0] = rospy.get_param("/ht1th/viewpara/robot_state")
+        viewpara[1] = rospy.get_param("/ht1th/viewpara/visual_nav")
+        viewpara[2] = rospy.get_param("/ht1th/viewpara/pose_x")
+        viewpara[3] = rospy.get_param("/ht1th/viewpara/pose_y")
+        viewpara[4] = rospy.get_param("/ht1th/viewpara/pose_yaw")
+        viewpara[5] = rospy.get_param("/ht1th/viewpara/vel_left")
+        viewpara[6] = rospy.get_param("/ht1th/viewpara/vel_right")
+        viewpara[7] = rospy.get_param("/ht1th/viewpara/vel_angle")
+        rospy.loginfo(viewpara)
+        msg = struct.pack("<8f",viewpara[0],viewpara[1],viewpara[2],viewpara[3],viewpara[4],viewpara[5],viewpara[6],viewpara[7])
+        client_socket.sendto(msg, server_address) #将msg内容发送给指定接收方
+        time.sleep(1)
+
+
+
+    
