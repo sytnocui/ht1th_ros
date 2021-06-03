94c01f8dbdb154d7bbac5b7185c459b11bce0523
sytnocui <1079851849@qq.com>
2021-06-04 02:17:57 +0800

    补充图像代码的设置参数服务器

--- qingzhou_ws/src/ht1th_pkg/scripts/udp_test.py
+++ qingzhou_ws/src/ht1th_pkg/scripts/udp_test.py
@@ -38,20 +38,35 @@ def udp_viewpara_send():
 def udp_img_send():
     pass
 #####################################################################################VISUAL
+#0-close   1-check_light   2-red_light   3-green_light  4-yellow_light  5-check_lane   6-lane
+
+#TODO:测试参数服务器设置时间
 def cam_get():
     pass
 
-def lane_check():
-    #初始化Twist类型的消息
-    vel_msg = Twist()
-    vel_msg.linear.x=0.5
-    vel_msg.angular.z=0.2
-    #发布消息
-    visual_vel_pub.publish(vel_msg)
-    rospy.loginfo("[%0.2f m/s,%0.2f rad/s]",vel_msg.linear.x,vel_msg.angular.z)
-
 def light_check():
     pass
+    if 0:
+        rospy.set_param("/ht1th/viewpara/visual_state",1)#结尾如果没检测到灯就赋成1
+    elif 1:
+        rospy.set_param("/ht1th/viewpara/visual_state",2)#检测到红灯赋为2
+    elif 2:
+        rospy.set_param("/ht1th/viewpara/visual_state",3)#检测到绿灯赋为3
+    elif 3:
+        rospy.set_param("/ht1th/viewpara/visual_state",4)#检测到黄灯赋为4
+
+def lane_check():
+    pass
+    if 0:
+        rospy.set_param("/ht1th/viewpara/visual_state",5)#结尾如果没检测到车道线就赋成5
+    elif 1:
+        rospy.set_param("/ht1th/viewpara/visual_state",6)#检测到车道线赋为6
+        #发布话题
+        vel_msg = Twist()
+        vel_msg.linear.x=0.5
+        vel_msg.angular.z=0.2
+        visual_vel_pub.publish(vel_msg)
+        rospy.loginfo("[%0.2f m/s,%0.2f rad/s]",vel_msg.linear.x,vel_msg.angular.z)
 
 #####################################################################################
 
@@ -70,17 +85,14 @@ if __name__=="__main__":
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        if 1:
-            #拍照
-            cam_get()
-            if 0:
-                #红绿灯检测
-                light_check()
-                pass
-            else:
-                #车道线检测
-                lane_check()
-                pass
+        #0-stop    1-towait     2-toload    3-tounload   4-reached   5-wrong
+        nav_state = rospy.get_param("/ht1th/viewpara/nav_state")#获取机器人状态
+        if nav_state == 1 or 3:
+            cam_get()#拍照
+            if nav_state == 3:#前往卸货区
+                light_check()#红绿灯检测
+            elif nav_state == 1:#前往等待区
+                lane_check()#车道线检测
         
         udp_thread()
 
