7208ae977537ae921dc22b0f3ad326e20f41c51d
sytnocui <1079851849@qq.com>
2021-06-04 01:20:10 +0800

    大改代码结构，把图像放在主线程，udp放在子线程，子线程不要循环

--- qingzhou_ws/src/ht1th_pkg/scripts/udp_test.py
+++ qingzhou_ws/src/ht1th_pkg/scripts/udp_test.py
@@ -12,6 +12,14 @@ import rospy
 from geometry_msgs.msg import Twist
 
 ####################################################################################UDP
+def udp_thread():#udp传输子线程函数
+    timer = threading.Thread(target=udp_send)
+    timer.start()
+
+def udp_send():
+    udp_viewpara_send()
+    udp_img_send()
+
 def udp_viewpara_send():
     #读取参数服务器
     viewpara[0] = rospy.get_param("/ht1th/viewpara/nav_state")
@@ -26,56 +34,55 @@ def udp_viewpara_send():
     msg = struct.pack("<8f",*viewpara)
     client_socket.sendto(msg, server_address) #将msg内容发送给指定接收方
 
-####################################################################################VISUAL
-def lane_detection_thread():#车道线线程开启或结束函数
-    #TODO:加入开启或关闭进程参数
-    timer = threading.Thread(target=lane_detection)
-    timer.start()
+def udp_img_send():
+    pass
+#####################################################################################VISUAL
+def cam_get():
+    pass
 
-def lane_detection():
-    #TODO: 加入车道线检测代码,不能把循环写死要留跳出条件
-    #初始化ros节点
-    rospy.init_node("ht1th_visual_lane",anonymous=True)
-    #创建一个Publisher,发布cmd_vel的topic,消息类型为geometry_msgs::Twist,没有队列
-    visual_vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=None)
-    #设置循环的频率
-    rate = rospy.Rate(10)
+def lane_check():
+    #初始化Twist类型的消息
+    vel_msg = Twist()
+    vel_msg.linear.x=0.5
+    vel_msg.angular.z=0.2
+    #发布消息
+    visual_vel_pub.publish(vel_msg)
+    rospy.loginfo("[%0.2f m/s,%0.2f rad/s]",vel_msg.linear.x,vel_msg.angular.z)
 
-    while not rospy.is_shutdown():
-        #初始化Twist类型的消息
-        vel_msg = Twist()
-        vel_msg.linear.x=0.5
-        vel_msg.angular.z=0.2
-		#发布消息
-        visual_vel_pub.publish(vel_msg)
-        rospy.loginfo("[%0.2f m/s,%0.2f rad/s]",vel_msg.linear.x,vel_msg.angular.z)
-        #按照循环频率延时
-        rate.sleep()
+def light_check():
+    pass
 
-def light_detection_thread():#红绿灯线程开启或结束函数
-    #TODO:加入开启或关闭进程参数
-    timer = threading.Thread(target=light_detection)
-    timer.start()
-
-def light_detection():
-    #TODO: 加入红绿灯检测代码,不能把循环写死要留跳出条件
-    #初始化ros节点
-    rospy.init_node("ht1th_visual_light",anonymous=True)
-    #设置循环的频率
-    rate = rospy.Rate(10)
-
-    while not rospy.is_shutdown():
-        #按照循环频率延时
-        rate.sleep()
 #####################################################################################
 viewpara = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
-#client 发送端初始化
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ("127.0.0.1", 8888)  # 接收方 服务器的ip地址和端口号
 
 #TODO:全局变量写在main下面行吗
 if __name__=="__main__":
+    #client 发送端初始化
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ("127.0.0.1", 8888)  # 接收方 服务器的ip地址和端口号
 
-    while True:
-        udp_viewpara_send()
+    #初始化ros节点
+    rospy.init_node("ht1th_visual",anonymous=True)
+
+    #创建一个Publisher,发布cmd_vel的topic,消息类型为geometry_msgs::Twist,没有队列
+    visual_vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=None)
+
+    #设置循环的频率
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        if 1:
+            #拍照
+            cam_get()
+            if 0:
+                #红绿灯检测
+                light_check()
+                pass
+            else:
+                #车道线检测
+                lane_check()
+                pass
+
+        #按照循环频率延时
+        rate.sleep()
